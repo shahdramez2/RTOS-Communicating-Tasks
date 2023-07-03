10f83cb395cd477dbe8a7660d122a1212429da54
nadeen2 <nadeenmohammed999@gmail.com>
2023-07-03 11:53:06 +0300

    First commit

---
+++ Demo/Debug/FreeRTOS Source/croutine.d
@@ -0,0 +1,22 @@
+FreeRTOS Source/croutine.o: ../FreeRTOS\ Source/croutine.c \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/FreeRTOS.h \
+ ../include/FreeRTOSConfig.h ../system/include/cmsis/system_stm32f4xx.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/projdefs.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/portable.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/deprecated_definitions.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/mpu_wrappers.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/task.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/list.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/croutine.h
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/FreeRTOS.h:
+../include/FreeRTOSConfig.h:
+../system/include/cmsis/system_stm32f4xx.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/projdefs.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/portable.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/deprecated_definitions.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/mpu_wrappers.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/task.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/list.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/croutine.h:
---
+++ Demo/Debug/FreeRTOS Source/event_groups.d
@@ -0,0 +1,28 @@
+FreeRTOS Source/event_groups.o: ../FreeRTOS\ Source/event_groups.c \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/FreeRTOS.h \
+ ../include/FreeRTOSConfig.h ../system/include/cmsis/system_stm32f4xx.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/projdefs.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/portable.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/deprecated_definitions.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/mpu_wrappers.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/task.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/list.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/timers.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/task.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/event_groups.h \
+ C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/timers.h
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/FreeRTOS.h:
+../include/FreeRTOSConfig.h:
+../system/include/cmsis/system_stm32f4xx.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/projdefs.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/portable.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/deprecated_definitions.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/mpu_wrappers.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/task.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/list.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/timers.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/task.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/event_groups.h:
+C:\Users\Nadeen\OneDrive\Desktop\RTOS_2025\Demo\Demo\FreeRTOS\ Source\include/timers.h:
